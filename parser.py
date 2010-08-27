772690e8c20ce337dcd9a612a834f834ef0c5e46
Jeremy <Jeremy@Elizabeth-Lockwoods-Computer.local>
2010-08-27 15:34:17 -0500

    Basic parsing

--- parser.py
+++ parser.py
@@ -1,7 +1,42 @@
+import sys
+
 state = None
 
-def next_token():
-	pass
+def _r():
+	state.r = sys.stdin.read(1)
+	return state.r
 
-def build_tree(input):
-	pass
+def _read_name():
+	nbuf = [ state.r ]
+	
+	state.r = _r()
+	while state.r.isalpha():
+		nbuf.append(_r())
+	
+	return ''.join(nbuf)
+
+def next_token():
+	while True:
+		if state.r in ' \t':
+			pass
+		elif state.r.isalpha() or state.r in '_?!': # name
+			return ('name', _read_name())
+		elif state.r.isdigit():
+			raise NotImplementedError("numbers aren't yet supported.")
+		elif state.r in "{}()"
+			_r()
+			return (state.r,)
+		elif state.r == '@':
+			_r()
+			return ('instvar', _read_name())
+		else:
+			return ("unknown", state.r)
+
+def stmt():
+	tok = next_token()
+	
+	if tok[0] == 'name':
+		if tok[1] == 'class': # check for keywords
+			pass
+		else: # method call
+			pass
--- runtime.py
+++ runtime.py
@@ -15,7 +15,25 @@ class State(threading.local):
 	
 	def lineno(self):
 		return self.contexts[-1][1]
+	
+	def push(self, ctx):
+		self.contexts.append(list(ctx))
+	
+	def pop(self):
+		self.contexts.pop()
+	
+	def incr_state(self):
+		self.contexts[-1][1] += 1
 
 def main(argv):
+	state.push(('(runtime)', 0))
+	
 	if len(argv) > 0:
-		
+		filename = argv.shift()
+		sys.stdin = open(filename, 'r')
+	else:
+		filename = '(stdin)'
+	
+	
+	
+	state.pop()
